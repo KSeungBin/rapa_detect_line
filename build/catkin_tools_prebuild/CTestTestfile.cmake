f6aa0131ca16cbeca74c3ef34e46696c48b1dda4
Im JH <withyou@empal.com>
2021-12-17 17:51:19 +0900

    123

---
+++ build/catkin_tools_prebuild/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/hans/Develops/rapa_detect_line/build/catkin_tools_prebuild
+# Build directory: /home/hans/Develops/rapa_detect_line/build/catkin_tools_prebuild
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_tools_prebuild/catkin_generated/setup_cached.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH='/home/hans/Develops/rapa_detect_line/devel/.private/catkin_tools_prebuild:/home/hans/Develops/test_ros1/rolling_ws/devel:/home/hans/Develops/test_ros1/test_ws/devel:/opt/ros/noetic:/home/hans/Develops/test_ros1/turtlebot_ws/devel'
+export LD_LIBRARY_PATH='/home/hans/Develops/test_ros1/rolling_ws/devel/lib:/home/hans/Develops/test_ros1/test_ws/devel/lib:/opt/ros/noetic/lib:/home/hans/Develops/test_ros1/turtlebot_ws/devel/lib'
+export PKG_CONFIG_PATH='/home/hans/Develops/test_ros1/rolling_ws/devel/lib/pkgconfig:/home/hans/Develops/test_ros1/test_ws/devel/lib/pkgconfig:/opt/ros/noetic/lib/pkgconfig:/home/hans/Develops/test_ros1/turtlebot_ws/devel/lib/pkgconfig'
+export PWD='/home/hans/Develops/rapa_detect_line/build/catkin_tools_prebuild'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/hans/Develops/rapa_detect_line/devel/.private/catkin_tools_prebuild/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/hans/Develops/rapa_detect_line/build/catkin_tools_prebuild:$ROS_PACKAGE_PATH"
